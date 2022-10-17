8f3cd66b493988d6d90bfd9b69944c2f29f37324
Dmitry Nagovitsyn <nagovicinde@mail.ru>
2022-10-18 00:40:51 +0400

    HomeWork2 added

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе. 
+// Функция должна на вход принимать число, а выдавать сумму его цифр
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int SumChifr(int chislo)
+{
+    int sum = 0;
+    while (chislo > 0)
+    {
+        sum = sum + (chislo % 10);
+        chislo = chislo / 10;
+    }
+    return sum;
+}
+
+int msg = Prompt("Введите число: ");
+int sum = SumChifr(msg);
+System.Console.WriteLine($"Суммой цифр веденного числа {msg} будет:  {sum}");

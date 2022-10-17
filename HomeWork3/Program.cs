6f21eab96908e0902a090a4fc3a2ef4e8b7aff08
Dmitry Nagovitsyn <nagovicinde@mail.ru>
2022-10-18 00:41:02 +0400

    HomeWork3 added

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 3: Напишите программу, которая задаёт массив из 8 случайных элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 7, 8
+
+Printer(Fill(8));
+
+int[] Fill(int Length)
+{
+    int[] array = new int[Length];
+    for (int i = 0; i <= array.Length - 1; i++)
+    {
+        int r = new Random().Next(-100, 100);
+        array[i] = r;
+
+    }
+    return array;
+}
+
+void Printer(int[] array)
+{
+    for (int i = 0; i <= array.Length - 1; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+}
